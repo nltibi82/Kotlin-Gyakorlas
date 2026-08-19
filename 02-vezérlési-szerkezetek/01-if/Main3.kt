06c0d04663511565c1764860d07d466f1b77dd67
Nádas Tibor László <nltibi82@gmail.com>
2026-08-19 15:39:34 +0200

    Kotlin: else-if gyakorlása

---
+++ 02-vezérlési-szerkezetek/01-if/Main3.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val age = 65
+    if (age<18) {
+        println("Gyerek")
+    }else if (age<65){
+        println("Felnőtt")
+    } else {
+        println("Nyugdíjas")
+    }
+}
