83b961c277cc237a6303e60a08637eae9c6ae8e5
Nádas Tibor László <nltibi82@gmail.com>
2026-08-19 16:51:35 +0200

    Kotlin: egyszerű jogosultságvizsgálat

---
+++ 02-vezérlési-szerkezetek/01-if/Main9.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val age = 25
+    val active = true    
+    if(active && age>=18){
+        println("Hozzáférés engedélyezve.")
+    } else {
+        println("Hozzáférés megtagadva.")
+    }
+}
