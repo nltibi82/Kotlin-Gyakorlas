a265c2961555d4a488e6c868603f6e3de1c05f99
Nádas Tibor László <nltibi82@gmail.com>
2026-08-19 16:12:16 +0200

    Kotlin: összehasonlító operátorok

---
+++ 02-vezérlési-szerkezetek/01-if/Main5.kt
@@ -0,0 +1,22 @@
+fun main(){
+    val a = 10
+    val b = 5
+    if (a < b) {
+        println("a kisebb mint b")
+    }
+    if (a > b) {
+            println("a nagyobb mint b")
+    }
+    if (a <= b) {
+        println("a kisebb vagy egyenlő mint b")
+    }
+    if (a >= b) {
+        println("a nagyobb vagy egyenlő mint b")
+    }
+    if (a == b) {
+        println("a egyenlő b")
+    }
+    if (a != b) {
+        println("a nem egyenlő b")
+    }
+}
