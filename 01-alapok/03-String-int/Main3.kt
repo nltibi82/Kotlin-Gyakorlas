a71880d59dfb68669def0bb44f11d8abd1de4193
Nádas Tibor László <nltibi82@gmail.com>
2026-08-19 14:42:53 +0200

    Kotlin: alapvető matematikai műveletek tesztelése

---
+++ 01-alapok/03-String-int/Main3.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val a = 10
+    val b = 3
+    println("a+b= ${a+b}")
+    println("a+b= $a+$b")
+    println("a+b= ${a}+${b}")
+    println("a-b= ${a-b}")
+    println("a-b= $a-$b")
+    println("a-b= ${a}-${b}")
+    println("a*b= ${a*b}")
+    println("a*b= $a*$b")
+    println("a*b= ${a}*${b}")
+    println("a/b= ${a/b}")
+    println("a/b= $a/$b")
+    println("a/b= ${a}/${b}")
+}
