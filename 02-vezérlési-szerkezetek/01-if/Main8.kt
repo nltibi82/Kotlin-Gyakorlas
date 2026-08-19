9258980f1bb9b7048842f3bfe5f2f372239609ea
Nádas Tibor László <nltibi82@gmail.com>
2026-08-19 16:44:40 +0200

    Kotlin: pozitív negatív nulla

---
+++ 02-vezérlési-szerkezetek/01-if/Main8.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val number = -7
+    if (number>0) {
+        println("Pozitív")
+    } else if (number<0) {
+        println("Negatív")
+    } else {
+        println("Nulla")
+    }
+}
