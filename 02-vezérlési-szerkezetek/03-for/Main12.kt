fdcdadc5a1f6dc9cd807c9f856e3f60f43e3ae74
Nádas Tibor <nltibi82@gmail.com>
2026-08-20 13:50:16 +0200

    Kotlin: karakter keresése Boolean változóval

---
+++ 02-vezérlési-szerkezetek/03-for/Main12.kt
@@ -0,0 +1,17 @@
+fun main(){
+    print("Adj meg egy nevet: ")
+    val name = readln()
+    print("Melyik karaktert keressem? ")
+    val searchChar = readln()[0]
+    var found = false
+    for (char in name){
+        if(char == searchChar){
+            found=true
+        }
+    }
+    if (found) {
+        println("Megtaláltam.")
+    } else {
+        println("Nincs benne.")
+    }
+}
