44b8698b65d30ab7ec358b0f99b9bb39985c2ba7
Nádas Tibor <nltibi82@gmail.com>
2026-08-20 17:23:05 +0200

    Kotlin: saját számbeolvasó függvény

---
+++ 03-függvények/01-alapok/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val number = readNumber()
+    println("Elfogadva: $number")
+}
+fun readNumber(): Double {
+    var number: Double?
+    do{
+        print("Adj meg egy számot: ")
+        number = readln().toDoubleOrNull()
+        if (number == null) {
+            println("Hibás szám!")
+        } 
+   } while (number == null)
+   return number
+}
