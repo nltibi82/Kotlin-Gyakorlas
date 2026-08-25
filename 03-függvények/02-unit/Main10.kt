ce1a1e50dc7418f7068583d2789f01aff591d22c
Nádas Tibor <nltibi82@gmail.com>
2026-08-25 13:14:26 +0200

    Kotlin: maximumot kereső függvény

---
+++ 03-függvények/02-unit/Main10.kt
@@ -0,0 +1,24 @@
+fun main() {
+    val num1 = readNumber()
+    val num2 = readNumber()
+
+    println("A nagyobb szám: ${max(num1, num2)}")
+}
+fun max(a: Double, b: Double): Double {
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+fun readNumber(): Double {
+    var number: Double?
+    do{
+        print("Adj meg egy számot: ")
+        number = readln().toDoubleOrNull()
+        if (number == null) {
+            println("Hibás szám!")
+        } 
+   } while (number == null)
+   return number
+}
