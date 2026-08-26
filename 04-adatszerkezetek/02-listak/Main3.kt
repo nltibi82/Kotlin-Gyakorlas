318a04021e3e41ae61060879638253674709af48
Nádas Tibor <nltibi82@gmail.com>
2026-08-26 16:15:32 +0200

    Kotlin: MutableList bővítése add metódussal

---
+++ 04-adatszerkezetek/02-listak/Main3.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val numbers = mutableListOf(10, 20, 30)
+
+    println("Eredeti lista: $numbers")
+
+    numbers.add(40)
+    numbers.add(50)
+
+    println("Bővített lista: $numbers")
+}
