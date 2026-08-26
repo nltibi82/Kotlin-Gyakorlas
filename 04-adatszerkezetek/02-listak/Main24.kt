3a8d1b5781ae427e0099db3c3bcbf382f563e70d
Nádas Tibor <nltibi82@gmail.com>
2026-08-26 17:39:20 +0200

    Kotlin: List sorrendjének megfordítása reversed metódussal

---
+++ 04-adatszerkezetek/02-listak/Main24.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val numbers = listOf(10, 20, 30, 40, 50)
+
+    val reversed = numbers.reversed()
+
+    println("Eredeti lista: $numbers")
+    println("Megfordított lista: $reversed")
+}
