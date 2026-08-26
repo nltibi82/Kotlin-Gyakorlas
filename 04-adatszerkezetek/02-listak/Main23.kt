f60eed298717f981ae813ef4f10dced382ca18b9
Nádas Tibor <nltibi82@gmail.com>
2026-08-26 17:36:22 +0200

    Kotlin: List rendezése csökkenő sorrendbe

---
+++ 04-adatszerkezetek/02-listak/Main23.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val numbers = listOf(50, 10, 40, 20, 30)
+
+    val sorted = numbers.sortedDescending()
+
+    println("Eredeti lista: $numbers")
+    println("Csökkenő sorrend: $sorted")
+}
