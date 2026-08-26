f2925fce440f312eddffb7cfef6cf9c6ae26dfbb
Nádas Tibor <nltibi82@gmail.com>
2026-08-26 15:27:50 +0200

    Kotlin: tömb létrehozása és indexelése

---
+++ 04-adatszerkezetek/01-tombok/Main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val numbers = arrayOf(10, 20, 30, 40, 50)
+
+    println("Első elem: ${numbers[0]}")
+    println("Harmadik elem: ${numbers[2]}")
+    println("Elemek száma: ${numbers.size}")
+}
