559fc0e98ca5a913c0368bcc22d11aee77b37128
Nádas Tibor <nltibi82@gmail.com>
2026-08-26 16:27:11 +0200

    Kotlin: List elemének keresése contains metódussal

---
+++ 04-adatszerkezetek/02-listak/Main6.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val numbers = mutableListOf(10, 20, 30, 40, 50)
+
+    println("Tartalmazza a 30-at: ${numbers.contains(30)}")
+    println("Tartalmazza a 100-at: ${numbers.contains(100)}")
+}
