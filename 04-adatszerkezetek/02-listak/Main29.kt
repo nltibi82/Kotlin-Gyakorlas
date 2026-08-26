02c1600bb2be3c2ccc02a3058992a002af5e1b1e
Nádas Tibor <nltibi82@gmail.com>
2026-08-26 18:00:55 +0200

    Kotlin: List első elemeinek kiválasztása take metódussal

---
+++ 04-adatszerkezetek/02-listak/Main29.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val numbers = listOf(10, 20, 30, 40, 50)
+
+    val firstThree = numbers.take(3)
+
+    println("Eredeti lista: $numbers")
+    println("Első három elem: $firstThree")
+}
