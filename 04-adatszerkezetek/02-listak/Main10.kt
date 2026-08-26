f6618767facde5d9fd122c9340a425a23a58fed1
Nádas Tibor <nltibi82@gmail.com>
2026-08-26 16:40:23 +0200

    Kotlin: lastIndexOf utolsó előfordulásának keresése

---
+++ 04-adatszerkezetek/02-listak/Main10.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val numbers = listOf(10, 20, 30, 20, 40, 20)
+
+    println("A 20 első indexe: ${numbers.indexOf(20)}")
+    println("A 20 utolsó indexe: ${numbers.lastIndexOf(20)}")
+}
