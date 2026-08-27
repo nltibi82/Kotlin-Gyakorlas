2b74782377e0f83da8995b13ce6098246f9f24a7
Nádas Tibor <nltibi82@gmail.com>
2026-08-27 14:07:06 +0200

    Kotlin: Set elemének keresése contains metódussal

---
+++ 04-adatszerkezetek/03-halamzok/Main3.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val numbers = setOf(10, 20, 30, 40, 50)
+
+    println("Tartalmazza a 30-at: ${numbers.contains(30)}")
+    println("Tartalmazza a 100-at: ${numbers.contains(100)}")
+}
