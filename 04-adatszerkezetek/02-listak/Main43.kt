8c3dde467c77ddeb5b5a1db97d68174a82479752
Nádas Tibor <nltibi82@gmail.com>
2026-08-27 10:58:06 +0200

    Kotlin: Map kulcsainak és értékeinek lekérése

---
+++ 04-adatszerkezetek/02-listak/Main43.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val ages = mapOf(
+        "Tibi" to 44,
+        "Péter" to 35,
+        "Anna" to 28
+    )
+
+    println("Kulcsok: ${ages.keys}")
+    println("Értékek: ${ages.values}")
+}
