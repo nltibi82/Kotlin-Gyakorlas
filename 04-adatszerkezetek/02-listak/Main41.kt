7a41fbdb8ebfe8f6aef7d9c3f4caa7d3067fd1cb
Nádas Tibor <nltibi82@gmail.com>
2026-08-27 10:51:46 +0200

    Kotlin: Map létrehozása és érték lekérése kulcs alapján

---
+++ 04-adatszerkezetek/02-listak/Main41.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val ages = mapOf(
+        "Tibi" to 44,
+        "Péter" to 35,
+        "Anna" to 28
+    )
+
+    println("Tibi életkora: ${ages["Tibi"]}")
+    println("Anna életkora: ${ages["Anna"]}")
+}
