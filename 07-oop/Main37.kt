1d3dbf543f58a836c193a9de0454729ed51f8718
Nádas Tibor <nltibi82@gmail.com>
2026-08-28 16:22:41 +0200

    Kotlin: object és singleton alapjainak bemutatása

---
+++ 07-oop/Main37.kt
@@ -0,0 +1,9 @@
+object AppConfig {
+    val appName = "Kotlin Gyakorlás"
+    val version = "1.0"
+}
+
+fun main() {
+    println("Alkalmazás: ${AppConfig.appName}")
+    println("Verzió: ${AppConfig.version}")
+}
