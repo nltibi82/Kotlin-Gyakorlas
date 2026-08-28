c6be4a4c46f6632d4a98f3ff00953d741891b753
Nádas Tibor <nltibi82@gmail.com>
2026-08-28 15:07:44 +0200

    Kotlin: metódus visszatérési értékkel

---
+++ 07-oop/Main7.kt
@@ -0,0 +1,12 @@
+class Person(val name: String, val age: Int) {
+
+    fun isAdult(): Boolean {
+        return age >= 18
+    }
+}
+
+fun main() {
+    val person = Person("Tibi", 44)
+
+    println("Felnőtt: ${person.isAdult()}")
+}
