e12e3f1cb2e701aa913d4e87fa229d3384a4fea7
Nádas Tibor László <nltibi82@gmail.com>
2026-08-29 13:49:17 +0200

    Kotlin: Student jegyek hozzáadása metódussal

--- 07-oop/Main49.kt
+++ 07-oop/Main49.kt
@@ -3,6 +3,9 @@ class Student (
     var age: Int, 
     var grades : MutableList<Int> = mutableListOf())
     {
+    fun addGrade(number: Int){
+        grades.add(number)
+    }
     fun introduce() {
         println("Szia, $name vagyok, $age éves.")
         println("Jegyek: ${grades}")
@@ -10,5 +13,8 @@ class Student (
 }
 fun main(){
     val student = Student("Tibi", 44)
+    student.addGrade(5)
+    student.addGrade(3)
+    student.addGrade(4)
     student.introduce()
 }
