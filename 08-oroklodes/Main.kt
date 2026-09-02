567b4351fcd192b72ccda7dcbbfe3d2ed142ac5e
Nádas Tibor László <nltibi82@gmail.com>
2026-09-02 12:50:25 +0200

    Kotlin: öröklődés és polimorfizmus

--- 08-oroklodes/Main.kt
+++ 08-oroklodes/Main.kt
@@ -2,10 +2,10 @@ fun main(){
     val student = Student("Tibi",44, "GDE")
     val person: Person = Student("Anna", 40, "BME")
     val teacher = Teacher("Anna", 35, "Kotlin")
+    val teacher2: Person = Teacher("Éva", 55, "BGE")
     student.introduce()
     student.study()
     person.introduce()
-    println("Név: ${teacher.name}")
-    println("Életkor: ${teacher.age}")
-    println("Tantárgy: ${teacher.subject}")
+    teacher.introduce()
+    teacher2.introduce()
 }
--- 08-oroklodes/Teacher.kt
+++ 08-oroklodes/Teacher.kt
@@ -1,3 +1,6 @@
 class Teacher(name: String, age: Int, var subject : String): Person(name,age){
-    
+    override fun introduce(){
+        super.introduce()
+        println("Tantárgy: ${subject}")
+    }
 }
