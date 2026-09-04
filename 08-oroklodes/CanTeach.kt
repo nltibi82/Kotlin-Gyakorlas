d3de27be942e08cd5d6376ba4d5ca4999654a093
Nádas Tibor László <nltibi82@gmail.com>
2026-09-04 14:36:12 +0200

    Kotlin: interface getter subject alapján

--- 08-oroklodes/CanTeach.kt
+++ 08-oroklodes/CanTeach.kt
@@ -1,6 +1,6 @@
 interface CanTeach{
     val subject: String
     val description: String 
-        get() = "Tanításra képes személy"
+        get() = "Tanításra képes személy, szakterület: ${subject}"
     fun teach()
 }
